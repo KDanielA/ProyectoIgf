45385980d322bf1185b4ee65cf0250fb7c6989d9
KDanielA <va18045@ues.edu.sv>
2020-10-07 18:20:49 -0600

    Funcionalidad limpiar agregada

---
+++ formulario.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+     //Acion boton limpiar
+     $('#btn-limpiar').on('click',function(){
+      //Limpiar Elementos
+      $('#formularioEstudiante')[0].reset();
+     });
+});
